0ddb8b0aa455332cffa504d44e2f8cad6e2bf93e
Oğuz Arslan <oguzarslan@Oguz-MacBook-Air.local>
2022-12-23 20:04:55 +0300

    first commit

---
+++ HR-Project/src/test/java/com/hrProject/HR/Project/HrProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrProject.HR.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
