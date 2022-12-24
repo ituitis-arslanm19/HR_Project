f9ba67e296579d76a9d7989e845b3b2973804249
ituitis-ak19 <bilalak1907@gmail.com>
2022-12-24 14:23:58 +0300

    db relationship test

--- HR-Project/src/main/java/com/hrProject/HR/Project/model/Employee.java
+++ HR-Project/src/main/java/com/hrProject/HR/Project/model/Employee.java
@@ -1,10 +1,32 @@
 package com.hrProject.HR.Project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import com.hrProject.HR.Project.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Data;
 
-@Entity
+import java.util.Date;
+
+@Entity(name="employee")
+@Data
 public class Employee {
     @Id
-    String id;
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="EMPLOYEE_ID")
+    private  String id;
+
+    @Column(name="EMPLOYEE_FIRSTNAME")
+    private String firstName;
+
+    @Column(name="EMPLOYEE_LASTNAME")
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @ManyToOne
+    @JoinColumn(name="DEPARTMENT_ID", referencedColumnName = "DEPARTMENT_ID")
+    private Department department;
 }
